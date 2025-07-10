052993b9b79eb9e7604ba5b645c635332104cc16
Gabriel00405 <gabriellongobardi5@gmail.com>
2025-07-10 18:14:17 -0300

    Add files via upload

---
+++ ufra/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ufra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
